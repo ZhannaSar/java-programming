ebd41c16008b84d2808bc1a20a5704e7c44e6f9e
ZhannaSar <zsarsen000@citymail.cuny.edu>
2021-07-06 11:09:24 -1000

    day59

--- src/day59_polymorphism_exceptions/InstanceOf.java
+++ src/day59_polymorphism_exceptions/InstanceOf.java
@@ -1,7 +1,16 @@
 package day59_polymorphism_exceptions;
 
+import day57_abstraction_polymorhism.shapes.Circle;
+import day57_abstraction_polymorhism.shapes.Shape;
+import day57_abstraction_polymorhism.shapes.Square;
+
 public class InstanceOf {
     public static void main(String[] args) {
+        Shape shape = new Circle();
+        System.out.println(shape instanceof Circle);
+        System.out.println(shape instanceof Square );
 
+
+        System.out.println(shape.getClass().getSimpleName());
     }
 }
